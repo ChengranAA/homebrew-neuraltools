5ee23615b82439f0b258f3cf5ed7f3ddd3ed1e77
ChengranAA <andrew_lcr@icloud.com>
2025-02-13 01:39:33 +0100

    update

---
+++ Casks/brainvoyager.rb
@@ -0,0 +1,20 @@
+cask "brainvoyager" do
+    version "23.2.0"
+    sha256 :no_check  # Use ':no_check' since the URL doesn't contain a checksum
+  
+    url "https://download.brainvoyager.com/bv/BrainVoyager_#{version}_arm64_Installer.pkg"
+    name "BrainVoyager"
+    desc "Neuroimaging analysis and visualization software"
+    homepage "https://www.brainvoyager.com/"
+  
+    pkg "BrainVoyager_#{version}_arm64_Installer.pkg", allow_untrusted: true
+  
+    uninstall pkgutil: "com.brainvoyager.pkg"
+  
+    zap trash: [
+      "~/Library/Preferences/com.braininnovation.BVNotebook.plist",
+      "~/Library/Caches/Brain Innovation", 
+      "~/Library/Saved Application State/com.braininnovation.BrainVoyager.savedState "
+    ]
+  end
+  
