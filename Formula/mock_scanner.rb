e354f57cb491d11331008125ad015d1dc3f9ca2d
ChengranAA <lcraaaaa@icloud.com>
2025-06-11 14:08:15 +0200

    add mock scanner sha

--- Formula/mock_scanner.rb
+++ Formula/mock_scanner.rb
@@ -2,6 +2,7 @@ class MockScanner < Formula
   desc "A mock scanner utility for psychology experiment testing."
   homepage "https://github.com/ChengranAA/mock_scanner"
   url "https://github.com/ChengranAA/mock_scanner/releases/download/0.0.1/mock_scanner"
+  sha256 "sha256:50c444879e618e61b1abf5093b592fb1346a5925d2a7753a938405c219770a3c"
   version "0.0.1"
 
   def install
