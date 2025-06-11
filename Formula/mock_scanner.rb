506144329e23af3a36a1bce88e9c31c7d1082098
ChengranAA <lcraaaaa@icloud.com>
2025-06-11 14:05:27 +0200

    add mock scanner

---
+++ Formula/mock_scanner.rb
@@ -0,0 +1,10 @@
+class MockScanner < Formula
+  desc "A mock scanner utility for psychology experiment testing."
+  homepage "https://github.com/ChengranAA/mock_scanner"
+  url "https://github.com/ChengranAA/mock_scanner/releases/download/0.0.1/mock_scanner"
+  version "0.0.1"
+
+  def install
+    bin.install "mock_scanner"
+  end
+end
